06fdd166c2d182662ea89dac5a170b176e391d0c
David Morrow <dperrymorrow@gmail.com>
2025-03-23 18:37:41 -0700

    remove slugify, attempt at cache

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -3,7 +3,8 @@ import { username } from "./consts";
 
 export async function getAllPosts(): Promise<Post[]> {
   const res: Response = await fetch(
-    `https://dev.to/api/articles?username=${username}&page=1&per_page=100`,
+    `https://dev.to/api/articles?username=${username}&version=4&page=1&per_page=100`,
+    { cache: "no-store" },
   );
 
   const data = await res.json();
@@ -11,11 +12,15 @@ export async function getAllPosts(): Promise<Post[]> {
 }
 
 export async function getPostBySlug(slug: string): Promise<PostDetails> {
-  const res: Response = await fetch(`https://dev.to/api/articles/${username}/${slug}`);
+  const res: Response = await fetch(`https://dev.to/api/articles/${username}/${slug}`, {
+    cache: "no-store",
+  });
   return res.json();
 }
 
 export async function getComments(postId: number): Promise<Comment[]> {
-  const res: Response = await fetch(`https://dev.to/api/comments?a_id=${postId}`);
+  const res: Response = await fetch(`https://dev.to/api/comments?a_id=${postId}`, {
+    cache: "no-store",
+  });
   return res.json();
 }
