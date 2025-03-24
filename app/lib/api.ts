2415a1e20ef0596de0be0340cb0a689514edaa95
David Morrow <dperrymorrow@gmail.com>
2025-03-24 10:48:23 -0700

    removing params that seemed to cause caching

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -2,13 +2,11 @@ import { Post, PostDetails, Comment } from "./types";
 import { username } from "./consts";
 
 export async function getAllPosts(): Promise<Post[]> {
-  const res: Response = await fetch(
-    `https://dev.to/api/articles?username=${username}&version=4&page=1&per_page=100`,
-    { cache: "no-store" },
-  );
+  const res: Response = await fetch(`https://dev.to/api/articles?username=${username}`, {
+    cache: "no-store",
+  });
 
-  const data = await res.json();
-  return data;
+  return res.json();
 }
 
 export async function getPostBySlug(slug: string): Promise<PostDetails> {
