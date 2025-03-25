e1e8eef5f4035c65b91f42ba9d559eec09fa32ad
David Morrow <dperrymorrow@gmail.com>
2025-03-25 09:49:22 -0700

    adding sitemap

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -2,23 +2,16 @@ import { Post, PostDetails, Comment } from "./types";
 import { username } from "./consts";
 
 export async function getAllPosts(): Promise<Post[]> {
-  const res: Response = await fetch(`https://dev.to/api/articles?username=${username}`, {
-    cache: "no-store",
-  });
-
+  const res: Response = await fetch(`https://dev.to/api/articles?username=${username}`);
   return res.json();
 }
 
 export async function getPostBySlug(slug: string): Promise<PostDetails> {
-  const res: Response = await fetch(`https://dev.to/api/articles/${username}/${slug}`, {
-    cache: "no-store",
-  });
+  const res: Response = await fetch(`https://dev.to/api/articles/${username}/${slug}`);
   return res.json();
 }
 
 export async function getComments(postId: number): Promise<Comment[]> {
-  const res: Response = await fetch(`https://dev.to/api/comments?a_id=${postId}`, {
-    cache: "no-store",
-  });
+  const res: Response = await fetch(`https://dev.to/api/comments?a_id=${postId}`);
   return res.json();
 }
---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://newfinds.com",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  outDir: "out",
+  // ...other options
+};
